04eebae6541a977fb863a11aa7d62129dad78c33
koju-r28 <kojur28@gmail.com>
2026-01-08 17:01:46 +0545

    installed react-vite and tailwind css

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold text-green-400">
+        SPENDWISE 🚀
+      </h1>
+    </div>
+  )
+}
+
+ 
+
+export default App
